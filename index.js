97ae998fc7795fb443b352fcc73afb0010b99f62
Marcos J. Alvarez <garusis@gmail.com>
2016-12-31 20:00:45 -0500

    Add support to attach multilevel scope functions

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ function test(operation) {
 app.use(function (req, res, next) {
 
     let attached = {
-        round: function (arg) {
-            return Math.round(arg);
-        },
-        number: function (arg) {
-            return parseInt(arg);
+        number: {
+            round: function (arg) {
+                return Math.round(arg);
+            },
+            parse: function (arg) {
+                return parseInt(arg);
+            }
         },
         lazy: function (arg) {
             return 'lazyyyy-' + arg;
@@ -27,15 +29,26 @@ app.use(function (req, res, next) {
     };
 
     const containsFunc = /(\w+)\(/;
-    req.query = _.transform(req.query, function (result, operation, key) {
-        if (containsFunc.test(operation)) {
-            operation = operation.replace(/(\w+)\(/g, 'attached.$1(');
-            return result[key] = eval(operation);
-        }
-        result[key] = operation;
-    });
+    let query = {};
+    let errorMessage = false;
 
-    next();
+    _.every(req.query, function (operation, key) {
+        if (containsFunc.test(operation)) {
+            operation = operation.replace(/((\.?\w+)+)\(/g, 'attached.$1(');
+            try {
+                return query[key] = eval(operation);
+            } catch (err) {
+                errorMessage = err.message;
+                return false;
+            }
+        }
+        query[key] = operation;
+        return true;
+    });
+    req.query = query;
+
+    if (errorMessage) res.status(406);
+    next(errorMessage);
 });
 
 app.get('/test', function (req, res) {
