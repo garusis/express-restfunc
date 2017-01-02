51f77889e0a062605306f20cc99df40574f8afc8
Marcos J. Alvarez <garusis@gmail.com>
2017-01-01 21:18:11 -0500

    Format proyect as standalone express module

--- index.js
+++ index.js
@@ -5,51 +5,15 @@
  */
 const express = require('express');
 const _ = require('lodash');
+const restfunc = require('./lib/index');
 
 const app = express();
 
-function test(operation) {
-    eval(operation);
-}
-
-app.use(function (req, res, next) {
-
-    let attached = {
-        number: {
-            round: function (arg) {
-                return Math.round(arg);
-            },
-            parse: function (arg) {
-                return parseInt(arg);
-            }
-        },
-        lazy: function (arg) {
-            return 'lazyyyy-' + arg;
-        }
-    };
-
-    const containsFunc = /(\w+)\(/;
-    let query = {};
-    let errorMessage = false;
-
-    _.every(req.query, function (operation, key) {
-        if (containsFunc.test(operation)) {
-            operation = operation.replace(/((\.?\w+)+)\(/g, 'attached.$1(');
-            try {
-                return query[key] = eval(operation);
-            } catch (err) {
-                errorMessage = err.message;
-                return false;
-            }
-        }
-        query[key] = operation;
-        return true;
-    });
-    req.query = query;
-
-    if (errorMessage) res.status(406);
-    next(errorMessage);
+restfunc.attach('lazy', function (arg) {
+    return 'lazyyyy-' + arg;
 });
+
+app.use(restfunc());
 
 app.get('/test', function (req, res) {
     res.json(req.query);
